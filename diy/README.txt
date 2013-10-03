2595efd51349c23720704fc85ce1f57a268582be
Luc Maranget <Luc.Maranget@inria.fr>
2013-10-03 15:17:15 +0000

    +
    git-svn-id: svn+ssh://svn-rsem019.cl.cam.ac.uk/herdtools@7 be346146-55b3-0410-be30-c33b057dc6c4

--- diy/README.txt
+++ diy/README.txt
@@ -2,14 +2,17 @@
 
 This is diy version a tool suite to test weak memory models.
 
-There are two subtools:
+We provide the following subtools
+ - herd: A generic simulator for weak memory models.
 
- - litmus: Given assembler programs for Power or X86 (a litmus test)
+ - litmus: Given assembler programs for Power, ARM or X86 (a litmus test)
            generates C programs, whose execution
            will test the memory model of the executing machine.
            
  - diy proper: Produce litmus tests from concise specifications.
 
+ - Some tools to analyse run logs of both herd and litmus.
+ 
 
 ** Home **
 http://diy.inria.fr/
@@ -28,11 +31,13 @@ See file INSTALL.txt
 
 ** Law **
 
+Herd authors are Jade Alglave and Luc Maranget.
+
 Litmus authors are Luc Maranget and Susmit Sarkar.
 
 Diy proper authors are Jade Alglave and Luc Maranget.
 
-Copyright 2010: Institut National de Recherche en Informatique et
+Copyright 2010--2013: Institut National de Recherche en Informatique et
 en Automatique, and the authors.
 
 Diy is released under the terms of the Gnu Lesser General Public License
--- diy/release.sh
+++ diy/release.sh
@@ -1,26 +1,27 @@
 set -e
 DIR=`dirname $0`
-REPOS=svn+ssh://secsvn@svn-sem.cl.cam.ac.uk/WeakMemory
+REPOS=svn+ssh://secsvn@svn-rsem019.cl.cam.ac.uk/herdtools
 . $DIR/version.sh
 . $DIR/funs.sh
 TMP=/var/tmp
-EXPORT=$TMP/exp.$$
-mkdir $EXPORT
-( cd $EXPORT && \
-  svn export -N $REPOS/mem.new && \
-  mkdir -p litmus.new && \
-  svn export -N $REPOS/litmus.new/plumbing litmus.new/plumbing && \
-  svn export -N $REPOS/gen.new && \
-  svn export -N $REPOS/diy )
 FINAL=$TMP/diy-$V
 /bin/rm -rf $FINAL && mkdir $FINAL
-( cd $EXPORT && tar chf - litmus.new gen.new ) | \
-( cd $FINAL && tar xf - ) && \
-( cd $FINAL && mv gen.new gen && \
-  mv litmus.new/plumbing litmus && rmdir litmus.new ) && \
-( cd $EXPORT && cp diy/*.txt diy/Makefile $FINAL )
-/bin/rm -rf $EXPORT
-( cleandir $FINAL/litmus ) && ( cleandir $FINAL/gen )
+( cd $FINAL && \
+  svn export -N $REPOS/litmus litmus && \
+  svn export -N $REPOS/gen gen && \
+  svn export -N $REPOS/herd herd && \
+  svn export -N $REPOS/tools tools && \
+  svn export -N $REPOS/lib lib && \
+  svn export -N $REPOS/diy/Makefile.herd Makefile && \
+  svn export -N $REPOS/diy/README.txt README.txt && \
+  svn export -N $REPOS/diy/INSTALL.txt INSTALL.txt && \
+  svn export -N $REPOS/diy/LICENSE.txt  && \
+  true
+)
+( cleandir $FINAL/litmus ) && \
+( cleandir $FINAL/gen ) && \
+( cleandir $FINAL/herd ) && \
+( cleandir $FINAL/tools )
 
 # add example and documentation
 sh $DIR/installdoc.sh $FINAL
