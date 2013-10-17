40276095e6e5dca5a9692869eb92be3fb428c647
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-10-17 11:29:08 +0000

    Forgot to add a file
    git-svn-id: svn+ssh://svn-rsem019.cl.cam.ac.uk/herdtools@32 be346146-55b3-0410-be30-c33b057dc6c4

---
+++ litmus.cpp/CLang.ml
@@ -0,0 +1,16 @@
+(*********************************************************************)
+(*                          Litmus                                   *)
+(*                                                                   *)
+(*     Jacques-Pascal Deplaix, INRIA Paris-Rocquencourt, France.     *)
+(*                                                                   *)
+(*  Copyright 2010 Institut National de Recherche en Informatique et *)
+(*  en Automatique and the authors. All rights reserved.             *)
+(*  This file is distributed  under the terms of the Lesser GNU      *)
+(*  General Public License.                                          *)
+(*********************************************************************)
+
+module Make(Tmpl:Template.S) = struct
+
+  let dump = assert false
+
+end
