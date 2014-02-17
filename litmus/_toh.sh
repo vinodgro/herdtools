01589ed304ccaa4aaf67b9c5049d9a4ae87e6136
maranget <Luc.Maranget@inria.fr>
2014-02-17 14:08:46 +0100

    - Adapt _tosh.sh script so as to handle gcc generated assembler with -c11 true

--- litmus/_toh.sh
+++ litmus/_toh.sh
@@ -2,7 +2,7 @@ FILE=$1
 cat <<EOF
 static void ass(FILE *out) {
 EOF
-awk '{printf("  fprintf(out,\"%%s\\n\",\"%s\");\n",$0) }' $1
+sed $1 -e 's|"|\\"|g' | awk '{printf("  fprintf(out,\"%%s\\n\",\"%s\");\n",$0) }'
 cat <<EOF
 }
 EOF
