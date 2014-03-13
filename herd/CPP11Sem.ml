3e85ec5b152b1c9fd3aa616e6b087d2ee2e87f71
John Wickerson <johnwickerson@cantab.net>
2014-03-13 17:30:07 +0000

    Improved CPP11 cat file. Ongoing work on semantics of CAS instructions. Fixed bug in same_proc related to init_writes. Fixed OpenCL cat file.

--- herd/CPP11Sem.ml
+++ herd/CPP11Sem.ml
@@ -46,13 +46,14 @@ module Make (C:Sem.Config)(V:Value.S)
       | CPP11.Pload(loc,reg,mo) ->
 	M.unitT (CPP11.maybev_to_location loc) >>=
 	(fun loc -> read_loc mo loc ii) >>= 
-	  (fun v -> write_reg reg v ii) >>! 
-	  B.Next
+	(fun v -> write_reg reg v ii) >>! 
+	B.Next
 
       | CPP11.Pstore(l,v,mo) ->
 	(M.unitT (CPP11.maybev_to_location l)) >>|
-	    (M.unitT (V.intToV (constant_to_int v))) >>=
-	(fun (loc, vv) -> write_loc mo loc vv ii) >>! B.Next
+	(M.unitT (V.intToV (constant_to_int v))) >>=
+	(fun (loc, vv) -> write_loc mo loc vv ii) >>! 
+        B.Next
 
       | CPP11.Plock l ->
 	(M.unitT (CPP11.maybev_to_location l)) >>=
@@ -67,6 +68,70 @@ module Make (C:Sem.Config)(V:Value.S)
 	(M.unitT (CPP11.maybev_to_location l)) >>=
 	  fun loc -> 
 	  M.mk_singleton_es (Act.Unlock loc) ii >>! B.Next
+
+(*
+Are we creating po-edges between the events as they
+are added to the event structure? (Hopefully yes.) 
+Same question for data/address dependencies. (The
+read of loc_exp should have a control dependency
+to all the other events that follow. (This control
+dependency isn't actually used though.) In the failure case
+there should be a data dependency from the atomic read of
+the object, to the non-atomic write of expected.) 
+
+For the strong case, we need to rule out the fail
+case when the values do match up.
+*)
+
+(*
+atomic_compare_exchange_weak_explicit(
+  A* object, 
+  C* expected, 
+  C desired, 
+  memory_order success, 
+  memory_order failure)
+
+	M.unitT (CPP11.maybev_to_location expected) >>=
+	(fun loc_exp -> read_loc na loc_exp ii) >>= 
+        (fun v_exp ->
+          M.altT
+            ((M.unitT (CPP11.maybev_to_location object)) >>=
+            (fun loc_obj -> read_loc failure loc_obj ii) >>=
+            (fun v_obj -> write_loc loc_exp v_obj) >>!
+            B.Next)
+            ((M.unitT (CPP11.maybev_to_location object)) >>|
+            (M.unitT (V.intToV (constant_to_int desired))) >>= 
+            (fun (loc_obj,v_des) -> 
+            rmw_loc success loc_obj v_exp v_des ii) >>!
+            B.Next))
+
+	M.unitT (CPP11.maybev_to_location expected) >>=
+	(fun loc_exp -> read_loc na loc_exp ii) >>= 
+        (fun v_exp ->
+          if v_exp != v_obj then
+            ((M.unitT (CPP11.maybev_to_location object)) >>=
+            (fun loc_obj -> read_loc failure loc_obj ii) >>=
+            (fun v_obj -> write_loc loc_exp v_obj) >>!
+            B.Next)
+          else
+            ((M.unitT (CPP11.maybev_to_location object)) >>|
+            (M.unitT (V.intToV (constant_to_int desired))) >>= 
+            (fun (loc_obj,v_des) -> 
+            rmw_loc success loc_obj v_exp v_des ii) >>!
+            B.Next))
+
+
+
+A successful CAS results in a non-atomic load of expected and a RMW of
+A. The value stored at the location addressed by the expected argument
+has to match the desired value (what terrible names!). The non-atomic
+load should be po and data-before the RMW.
+
+A failed CAS results in three memory accesses: a non-atomic load of
+expected, an atomic load of A, and a non-atomic write of expected.
+This time, the three accesses should be ordered by po and data in
+sequence: NA-load, atomic-load, NA-store.
+*)
 
       | CPP11.Pcas(l,v1,v2,mo_success, _ (*mo_failure *)) ->
 	 (M.unitT (CPP11.maybev_to_location l)) >>|
