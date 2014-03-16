d837b763b22f23e312899e2a1c8c7cd2e115bf78
maranget <Luc.Maranget@inria.fr>
2014-03-16 01:40:25 +0100

    Oups

--- litmus/_toh.sh
+++ litmus/_toh.sh
@@ -2,7 +2,7 @@ FILE=$1
 cat <<EOF
 static void ass(FILE *out) {
 EOF
-sed $1 -e 's|"|\\"|g' | awk '{printf("  fprintf(out,\"%%s\\n\",\"%s\");\n",$0) }'
+sed -e 's|"|\\"|g' $1 | awk '{printf("  fprintf(out,\"%%s\\n\",\"%s\");\n",$0) }'
 cat <<EOF
 }
 EOF
