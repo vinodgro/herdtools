89bdee2155ea3d53bd3ddabf21e3f692efded139
maranget <Luc.Maranget@inria.fr>
2014-04-29 23:01:00 +0200

    Correct doc building, which was wrong

--- diy/doc/mkpstex.sh
+++ diy/doc/mkpstex.sh
@@ -3,8 +3,7 @@ BASE=$1
 FONTSZ=$2
 COM=$3
 OPTS="$4"
-$COM $OPTS -Tfig $BASE.dot |\
-perl -pe 's/^(4)(\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+)\s4\s(.*)$/$1$2 2 $3/'  > $BASE.fig
+$COM $OPTS -Tfig $BASE.dot > $BASE.fig
 fig2dev -L pstex $BASE.fig $BASE.pstex
 fig2dev -L pstex_t -p $BASE.pstex -s $FONTSZ $BASE.fig |\
 sed -e 's@generated/@@' > $BASE.pstex_t
