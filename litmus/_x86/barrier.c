9cc9f541731a3fdeb20b96fcf404c90580802013
maranget <Luc.Maranget@inria.fr>
2014-08-01 18:21:33 +0200

    litmus: correct x86 barrier

--- litmus/_x86/barrier.c
+++ litmus/_x86/barrier.c
@@ -15,27 +15,27 @@ static void barrier_init(sense_t *p, int n) {
 
 static void barrier_wait(sense_t *p) {
   int sense = p->sense ;
-  int r1 ;
+  int r1,r2 ;
   asm __volatile__ (
     "movl $-1,%[eax]\n\t"
     "lock xaddl %[eax],%[c]\n\t"
     "subl $1,%[eax]\n\t"
-    "je 0f\n\t"
+    "je 0f\n"
     "1:\n\t"
     "cmpl %[ms],%[s]\n\t"
     "je 1b\n\t"
-    "jmp 2f\n\t"
+    "jmp 2f\n"
     "0:\n\t"
-    "movl %[np],%[c]\n\t"
+    "movl %[np],%[r2]\n\t"
+    "movl %[r2],%[c]\n\t"
     "xorl %[eax],%[eax]\n\t"
     "testl %[ms],%[ms]\n\t"
     "sete %%al\n\t"
     "movl %[eax],%[s]\n\t"
-    "mfence\n\t"
-    "jmp 3f\n\t"
+    "mfence\n"
     "2:\n\t"
-    : [eax] "=&a" (r1)
-    : [ms] "r" (sense), [s] "m" (p->sense), [c] "m" (p->c), [np] "r" (p->n)
+    : [eax] "=&a" (r1), [r2] "=&r" (r2)
+    : [ms] "r" (sense), [s] "m" (p->sense), [c] "m" (p->c), [np] "m" (p->n)
     : "memory") ;
 }
 
