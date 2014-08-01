bb408f62e031dea22868cfcf040c9fc8392f2cfb
maranget <Luc.Maranget@inria.fr>
2014-08-01 18:39:22 +0200

    litmus: spare one register in x86 barrier.

--- litmus/_x86/barrier.c
+++ litmus/_x86/barrier.c
@@ -15,7 +15,7 @@ static void barrier_init(sense_t *p, int n) {
 
 static void barrier_wait(sense_t *p) {
   int sense = p->sense ;
-  int r1,r2 ;
+  int r1 ;
   asm __volatile__ (
     "movl $-1,%[eax]\n\t"
     "lock xaddl %[eax],%[c]\n\t"
@@ -26,15 +26,15 @@ static void barrier_wait(sense_t *p) {
     "je 1b\n\t"
     "jmp 2f\n"
     "0:\n\t"
-    "movl %[np],%[r2]\n\t"
-    "movl %[r2],%[c]\n\t"
+    "movl %[np],%[eax]\n\t"
+    "movl %[eax],%[c]\n\t"
     "xorl %[eax],%[eax]\n\t"
     "testl %[ms],%[ms]\n\t"
     "sete %%al\n\t"
     "movl %[eax],%[s]\n\t"
     "mfence\n"
     "2:\n\t"
-    : [eax] "=&a" (r1), [r2] "=&r" (r2)
+    : [eax] "=&a" (r1)
     : [ms] "r" (sense), [s] "m" (p->sense), [c] "m" (p->c), [np] "m" (p->n)
     : "memory") ;
 }
